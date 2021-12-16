9b9fc6e7655e55779eb9eb9333f5ecbf370ef405
cristiannd <cristian.donalicio@gmail.com>
2021-12-16 16:31:54 -0300

    part 1.3

--- src/index.js
+++ src/index.js
@@ -3,28 +3,36 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  }
 
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises2: 7,
+  }
+
+  const part3 = {
+    exercises3: 14,
+    name: 'State of a component',
+  }
+  
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        name1={part1.name}
+        name2={part2.name}
+        name3={part3.name}
+        exercises1={part1.exercises1}
+        exercises2={part2.exercises2}
+        exercises3={part3.exercises3}
       />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises1}
+        exercises2={part2.exercises2}
+        exercises3={part3.exercises3}
       />
     </div>
   )
@@ -46,7 +54,9 @@ const Content = (props) => {
 
 const Part = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
 }
 
